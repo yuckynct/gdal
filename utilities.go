6cbb693585ede2803f83b5523a2c5fce98a325d1
albrazeau <abrazeau@dewberry.com>
2020-10-13 09:55:21 -0400

    add gdal info bindings

--- utilities.go
+++ utilities.go
@@ -170,3 +170,22 @@ func Rasterize(dstDS string, sourceDS Dataset, options []string) (Dataset, error
 	return Dataset{ds}, nil
 
 }
+
+func Info(sourceDS Dataset, options []string) string {
+	length := len(options)
+	opts := make([]*C.char, length+1)
+	for i := 0; i < length; i++ {
+		opts[i] = C.CString(options[i])
+		defer C.free(unsafe.Pointer(opts[i]))
+	}
+	opts[length] = (*C.char)(unsafe.Pointer(nil))
+	infoopts := C.GDALInfoOptionsNew(
+		(**C.char)(unsafe.Pointer(&opts[0])),
+		(*C.GDALInfoOptionsForBinary)(unsafe.Pointer(nil)))
+	defer C.GDALInfoOptionsFree(infoopts)
+
+	info := C.GDALInfo(sourceDS.cval, infoopts)
+
+	return C.GoString(info)
+
+}
